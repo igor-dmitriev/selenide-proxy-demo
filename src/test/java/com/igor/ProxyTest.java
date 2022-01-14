3f9bcfa74342730271ee8f07e508178a3b540c68
Ihor Dmitriiev <i.dmitriiev@spd-ukraine.com>
2022-01-14 09:07:56 +0200

    check network logs

--- src/test/java/com/igor/ProxyTest.java
+++ src/test/java/com/igor/ProxyTest.java
@@ -1,15 +1,26 @@
 package com.igor;
 
-import com.browserup.bup.BrowserUpProxy;
-import com.browserup.bup.proxy.CaptureType;
-import com.browserup.harreader.model.HarEntry;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.List;
+import java.util.logging.Level;
 
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class ProxyTest {
@@ -17,25 +28,33 @@ class ProxyTest {
   void test() {
     Configuration.timeout = 15000;
     Configuration.browserSize = "1920x1080";
-    Configuration.holdBrowserOpen = true;
-    Configuration.proxyEnabled = true;
 
-    WebDriverRunner.getAndCheckWebDriver(); // otherwise WebDriverRunner.getSelenideProxy() returns null
+    ChromeOptions options = new ChromeOptions();
+    LoggingPreferences logPrefs = new LoggingPreferences();
+    logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
 
-    BrowserUpProxy proxy = WebDriverRunner.getSelenideProxy().getProxy();
-    proxy.setHarCaptureTypes(CaptureType.getAllContentCaptureTypes());
-    proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
-    proxy.newHar("pofig");
-    open("https://google.com");
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("goog:loggingPrefs", logPrefs);
+    capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
-    List<HarEntry> requests = proxy.getHar().getLog().getEntries();
-    for (HarEntry request : requests) {
-      System.out.println("URL: " + request.getUrl());
-      System.out.println("Status: " + request.getResponse().getStatus());
-      System.out.println("Status text: " + request.getResponse().getStatusText());
-      System.out.println("Body: " + request.getResponse().getContent().getText());
-      System.out.println("*****************************************");
+    Configuration.browserCapabilities = capabilities;
+
+    open("https://my.smartcrowd.ae/property-browse");
+    $(byText("Live and Coming Soon Properties")).shouldBe(Condition.visible);
+
+    LogEntries logs = WebDriverRunner.getWebDriver().manage().logs().get(LogType.PERFORMANCE);
+    String logsBrowser = "";
+
+    for (LogEntry le : logs) {
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
+      JsonParser jp = new JsonParser();
+      JsonElement je = jp.parse(le.getMessage());
+
+      if (gson.toJson(je).contains("api.my")) {
+        logsBrowser = logsBrowser + gson.toJson(je);
+      }
     }
+    System.out.println(logsBrowser);
 
   }
 }
